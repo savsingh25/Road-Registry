3c7c275de22de6fbd56f079c2f03f15a97925f5c
savsingh25 <145418303+savsingh25@users.noreply.github.com>
2025-06-04 21:21:33 +1000

    Update AddDemeritPointsTest.java

--- src/test/java/AddDemeritPointsTest.java
+++ src/test/java/AddDemeritPointsTest.java
@@ -13,7 +13,7 @@ public class AddDemeritPointsTest {
 
     @Test
     public void testValidUnder21Suspended() {
-        Person p = new Person("user1", "15-11-2006");
+        Person p = new Person("23!!abcdXY", "15-11-2006");
         p.addDemeritPoints("01-01-2024", 3);
         p.addDemeritPoints("01-06-2025", 4);
         assertTrue(p.isSuspended());
@@ -21,7 +21,7 @@ public class AddDemeritPointsTest {
 
     @Test
     public void testValidOver21NotSuspended() {
-        Person p = new Person("user2", "15-11-1990");
+        Person p = new Person("24@@idAB", "15-11-1990");
         p.addDemeritPoints("01-01-2024", 6);
         p.addDemeritPoints("01-06-2025", 5);
         assertFalse(p.isSuspended());
@@ -29,21 +29,21 @@ public class AddDemeritPointsTest {
 
     @Test
     public void testInvalidDateFormat() {
-        Person p = new Person("user3", "15-11-1990");
+        Person p = new Person("29##xxCD", "15-11-1990");
         String result = p.addDemeritPoints("2024/05/01", 3);
         assertEquals("Failed", result);
     }
 
     @Test
     public void testInvalidPointValue() {
-        Person p = new Person("user4", "15-11-1990");
+        Person p = new Person("88&&badDD", "15-11-1990");
         String result = p.addDemeritPoints("01-05-2024", 10);
         assertEquals("Failed", result);
     }
 
     @Test
     public void testSuspensionOver21() {
-        Person p = new Person("user5", "15-11-1990");
+        Person p = new Person("24@@idEE", "15-11-1990");
         p.addDemeritPoints("01-01-2023", 6);
         
         p.addDemeritPoints("01-06-2024", 7);
