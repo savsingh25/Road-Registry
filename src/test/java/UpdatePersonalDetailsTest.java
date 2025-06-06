f875f15b8a9bb61d123e3c57ada5d074e658274b
savsingh25 <145418303+savsingh25@users.noreply.github.com>
2025-06-06 11:58:29 +1000

    Update UpdatePersonalDetailsTest.java

--- src/test/java/UpdatePersonalDetailsTest.java
+++ src/test/java/UpdatePersonalDetailsTest.java
@@ -1,8 +1,8 @@
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UpdatePersonalDetailsTest {
+
     // Sample person object used for calling methods
     Person person = new Person("dummyID", "01-01-1990");
 
@@ -10,17 +10,17 @@ public class UpdatePersonalDetailsTest {
      * Test case: Update should fail when the old ID is not found in the file.
      * This makes sure only existing users can be updated.
      */
-@Test
-public void testUpdateFailsWhenOldIdNotFound() {
-    boolean result = person.updatePersonalDetails(
-        "ZZ99notFound",
-        "ZZ99notFound",
-        "123|Fake St|Melb|Victoria|Australia",
-        "01-01-1990"
-    );
+    @Test
+    public void testUpdateFailsWhenOldIdNotFound() {
+        boolean result = person.updatePersonalDetails(
+            "ZZ99notFound",
+            "ZZ99notFound",
+            "123|Fake St|Melb|Victoria|Australia",
+            "01-01-1990"
+        );
+        assertFalse(result);
+    }
 
-    assertFalse(result);
-}
     /**
      * This should fail because minors are not allowed to change their address.
      */
@@ -34,7 +34,6 @@ public void testUpdateFailsWhenOldIdNotFound() {
             "201|Other Rd|Geelong|Victoria|Australia",
             "01-01-2010"
         );
-
         assertFalse(result);
     }
 
@@ -42,16 +41,16 @@ public void testUpdateFailsWhenOldIdNotFound() {
      * Add a valid person to the file.
      * This test checks if the person is added successfully.
      */
-@Test
-public void testAddValidPersonOnly() {
-    String personId = "23!!abcdXY";
-    String address = "101|Main St|Melbourne|Victoria|Australia";
-    String birthdate = "12-12-1990";
+    @Test
+    public void testAddValidPersonOnly() {
+        String personId = "23!!abcdXY";
+        String address = "101|Main St|Melbourne|Victoria|Australia";
+        String birthdate = "12-12-1990";
 
-    boolean result = person.addPerson(personId, address, birthdate);
+        boolean result = person.addPerson(personId, address, birthdate);
 
-    assertTrue(result, "合法人员应成功添加");
-}
+        assertTrue(result, "合法人员应成功添加");
+    }
 
     /**
      * Changing both the address and birthdate at the same time.
@@ -67,14 +66,13 @@ public void testAddValidPersonOnly() {
             "401|Oak St|Bendigo|Victoria|Australia",
             "02-02-1992"
         );
-
         assertFalse(result);
     }
 
     /**
      * Trying to change the ID when it starts with an even digit.
      * System rules say you can't change the ID if it starts with an even number.
-     * So this test should fail
+     * So this test should fail.
      */
     @Test
     public void testChangeIDWhenStartsWithEvenDigitShouldFail() {
@@ -86,7 +84,7 @@ public void testAddValidPersonOnly() {
             "500|Main St|Shepparton|Victoria|Australia",
             "01-01-1990"
         );
-
         assertFalse(result);
     }
 }
+
